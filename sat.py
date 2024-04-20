b488bf0183c5b59ad3501f7778b9abbc8cbdac6f
SIKASZZU <soots.sigvrid@gmail.com>
2024-04-20 13:14:08 +0300

    questions and thoughts

---
+++ sat.py
@@ -0,0 +1,48 @@
+# true or false variables
+A = True
+B = True
+# C = True
+
+# amount of ANDs, ORs, NOTs
+lo_AND = int
+lo_OR = int
+lo_NOT = int
+
+if ... and ...:
+    ...
+
+if ... or ...:
+    ...
+
+if not:
+    ...
+
+
+# 1. len(queue)
+# 2. count ANDs, ORs, NOTs
+# 3. v2lja m6elda, et kui or on enne andi, kas siis saab and olla true # saab kyll
+    # T6en2osusega arvetama. Kui isegi 0.1 on p2rast algorithmi, siis on vastus True.
+
+# kysimused:
+    # kui sedasi lahendada, kas loeb mitu variabli on?
+    # kuidas v2lja m6elda, kui suur saab queue lenght yldse olla
+
+
+
+
+# examples
+
+# FV = variable is false
+# TV = vairiable is true
+
+# 2 FV, 2 NOT, 1 AND or 1 OR == TRUE
+    # different variations
+
+# 1FV, 1TV
+# 2 TV
+
+
+
+
+
+
